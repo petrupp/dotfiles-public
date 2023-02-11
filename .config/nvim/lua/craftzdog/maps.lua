8a1ccc885c7f08f279c062d0eb2b04b15433ae1b
petrupp <petru@naver.com>
2023-02-11 17:19:17 +0900

    Update maps.lua

--- .config/nvim/lua/craftzdog/maps.lua
+++ .config/nvim/lua/craftzdog/maps.lua
@@ -32,3 +32,6 @@ keymap.set('n', '<C-w><left>', '<C-w><')
 keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+
+--nerd tree
+keymap.set('n', '<C-n>', ':NERDTreeToggle<CR>')
